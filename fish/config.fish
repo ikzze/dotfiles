12766b259c3b3a79bd8ee87300664f0d68b2ec19
ikzze <nova2009om@gmail.com>
2026-04-28 20:05:50 +0400

    Update config.fish

--- fish/config.fish
+++ fish/config.fish
@@ -12,10 +12,4 @@ alias ls='eza -A -s modified -r'
 
 starship init fish | source
 
-set -U fish_pager_color_completion white
-
-set -e fish_color_normal
-
-set fish_pager_color_prefix white
-
 set -gx GSK_RENDERER gl
